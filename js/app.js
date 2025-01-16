0e79c0ba1ed9767db836009a820086527acdee26
zinzinthin <zinzinthinzaw@gmail.com>
2025-01-16 07:26:40 +0630

    pressure

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const lon = "97.3462055";
 
 const key = "3e47c1570e1e6c8480182d878001cdf0";
 const uri = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
-console.log(uri);
+
 fetch(uri)
     .then(response => response.json())
     .then(data => displayWeatherData(data.list))
@@ -21,8 +21,14 @@ function displayWeatherData(items) {
         item.main.temp
     ]));
 
+    const pressureData = items.map(item => ([
+        item.dt * 1000,
+        item.main.pressure
+    ]));
+
     createHumidityChart(humidityData);
     createTempChart(temperatureData);
+    createPressureChart(pressureData);
 }
 
 function createHumidityChart(items) {
@@ -169,6 +175,22 @@ function createTempChart(items) {
         },
         plotOptions: {
             area: {
+
+                dataLabels: {
+                    enabled: true,
+                    align: 'right',
+                    formatter: function () {
+                        if (this.point.x === this.series.data[this.series.data.length - 1].x) {
+                            return this.series.name;
+                        }
+                        return null;
+                    },
+                    style: {
+                        color: "#fff",
+                        fontWeight: 'bloder',
+                        fontSize: 16
+                    },
+                },
                 
                 marker: {
                     enabled: false,
@@ -185,7 +207,96 @@ function createTempChart(items) {
         series: [{
             name: 'temperature',
             data: items,
-            color: 'brown',
+            color: '#e0225b',
+            lineWidth : 5
+        },
+    ]
+    });
+}
+
+function createPressureChart(items) {
+
+    Highcharts.chart('pressure', {
+        chart: {
+            type: 'area',
+            backgroundColor : 'transparent',
+        },
+       
+        title: {
+            floating : false,
+            align : 'left',
+            text: 'Average Weekly Pressure',
+            style : {
+                color : '#fff',
+            }
+        },
+       
+        xAxis: {
+            type: 'datetime',
+            labels: {
+                style: {
+                    color: "#FFF",
+                }
+            },
+
+            dateTimeLabelFormats: {
+                day: '%b %e',
+            },
+            tickInterval: 12 * 3600 * 1000,
+
+        },
+        yAxis: {
+            title: {
+                text: 'Pressure',
+                style : {
+                    color : '#fff',
+                }
+            },
+            labels: {
+                style: {
+                    color: "#FFF",
+                }
+            },
+            gridLineColor : 'transparent',
+        },
+        tooltip: {
+            pointFormat: '<span style="color:{point.color}">\u25CF </span>{series.name} : <b>{point.y}%</b>'
+        },
+        plotOptions: {
+            area: {
+
+                dataLabels: {
+                    enabled: true,
+                    align: 'right',
+                    formatter: function () {
+                        if (this.point.x === this.series.data[this.series.data.length - 1].x) {
+                            return this.series.name;
+                        }
+                        return null;
+                    },
+                    style: {
+                        color: "#fff",
+                        fontWeight: 'bloder',
+                        fontSize: 16
+                    },
+                },
+                
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'pressure',
+            data: items,
+            color: 'darkred',
             lineWidth : 5
         },
     ]
